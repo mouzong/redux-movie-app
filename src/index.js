c1cade72e5e04903eb90d92c65ac447ad61e4e3e
mouzong <andreasmouzong@gmail.com>
2021-05-20 21:35:14 +0100

    separated the redux logic in individual files

--- src/index.js
+++ src/index.js
@@ -3,40 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
+import allReducers from "./redux/reducers";
+import { Provider } from "react-redux";
 
 // STORE : GLOBALIZED STATE
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// ACTION INCREMENTATION
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//REDUCERS
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-//DISPATCH
-let store = createStore(counter);
-
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(decrement());
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
